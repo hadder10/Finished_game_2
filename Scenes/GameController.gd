15e1e2ee4fd5ec6765724707a4216f770a50b553
MoustachedMamba <113217632+MoustachedMamba@users.noreply.github.com>
2023-11-12 17:28:16 +0300

    фывадлфоывлда

--- Scenes/GameController.gd
+++ Scenes/GameController.gd
@@ -6,13 +6,17 @@ var living_npcs: int = 0
 var npc_list := []
 
 @export var current_level: PackedScene
-
+@export var rumble: AudioStreamPlayer
 
 func _ready():
+	rumble.play()
 	add_child(current_level.instantiate())
 	print(get_tree().get_nodes_in_group("NPC"))
 	for npc in get_tree().get_nodes_in_group("NPC"):
 		npc_list.append(npc)
+
+
+
 
 
 func _on_death():
@@ -34,3 +38,7 @@ func check_win_lose_on_timer_end():
 			return
 	win()
 	return
+
+
+func manage_musci():
+	pass
